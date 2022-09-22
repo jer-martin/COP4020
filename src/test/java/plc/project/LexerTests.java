2fd6ed7c4f5f20fbab41abd5a54b4b35f8de339b
jer-martin <48764226+jer-martin@users.noreply.github.com>
2022-09-22 11:24:17 -0400

    little tiny bux fix oopsies

--- src/test/java/plc/project/LexerTests.java
+++ src/test/java/plc/project/LexerTests.java
@@ -106,7 +106,7 @@ public class LexerTests {
                 Arguments.of("Character", "(", true),
                 Arguments.of("Comparison", "!=", true),
                 Arguments.of("Space", " ", false),
-                Arguments.of("Tab", "\t", false),
+                Arguments.of("Tab", "\t", false)
                 //Arguments.of("Mixed Equals", "!====", true)
         );
     }
